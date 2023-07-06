db75f0feed1d48cbc4c457bed8dd797256873746
JakBul <bjakub68@gmail.com>
2023-07-06 17:53:56 +0000

    Add quiz questions to questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which player has the most Premier League appearances?",
+        answer: "C. Gareth Barry",
+        options: [
+            "A. Frank Lampard",
+            "B. John Terry",
+            "C. Gareth Barry",
+            "D. Ryan Giggs"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which player has the most Premier League goals?",
+        answer: "A. Alan Shearer",
+        options: [
+            "A. Alan Shearer",
+            "B. Andy Cole",
+            "C. Thierry Henry",
+            "D. Wayne Rooney"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which player has the most Premier League assists?",
+        answer: "D. Ryan Giggs",
+        options: [
+            "A. Kevin De Bruyne",
+            "B. Cesc Fábregas",
+            "C. Frank Lampard",
+            "D. Ryan Giggs"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which team has won most most Premier League titles?",
+        answer: "B. Manchester United",
+        options: [
+            "A. Liverpool",
+            "B. Manchester United",
+            "C. Chelsea",
+            "D. Arsenal"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which player has the most Premier League hat-tricks?",
+        answer: "D. Sergio Agüero",
+        options: [
+            "A. Cristiano Ronaldo",
+            "B. Wayne Rooney",
+            "C. Luis Suárez",
+            "D. Sergio Agüero"
+        ]
+    }
+];
